21e7e105982575c8c3f3e03a21ce659828b2c890
Zhifa Qiu <zhifaq@Zhifas-Air.bbrouter>
2024-08-06 10:13:40 +1000

    updated metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Tutorials from "@/components/Tutorials";
 import Experience from "@/components/Experience";
-import Articles from "@/components/Articles";
 import SideHustles from "@/components/SideHustles";
 import Recommendations from "@/components/Recommendations";
 import Newsletter from "@/components/Newsletter";
 
 export const metadata = {
-  title: "Home - DevFolio",
-  description: "Page description",
+  title: "Zhifa's DevFolio",
+  description: "",
 };
 
 export default async function Home() {
